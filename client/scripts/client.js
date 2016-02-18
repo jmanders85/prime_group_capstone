36ec8addd444404c8be21f8b47b1a23d4e26c85d
nataliekoch <natalie.koch927@gmail.com>
2016-02-18 14:35:53 -0600

    added assets, calendar, and new reservation pages. Updated home.html with links to respected pages. Added UI-routing to all current pages

--- client/scripts/client.js
+++ client/scripts/client.js
@@ -3,19 +3,49 @@ var app = angular.module('sportApp', ['ui.router']);
 app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider){
   $stateProvider
     .state('login', {
-     url: '/',
-     templateUrl: 'views/login.html'
-   }),
-   $stateProvider
-     .state('home', {
+      url: '/',
+      templateUrl: 'views/login.html',
+      controller: 'LoginController'
+    })
+    .state('home', {
       url: '/home',
       templateUrl: 'views/home.html',
       controller: 'HomeController'
-     })
-
+    })
+    .state('assets', {
+      url: '/assets',
+      templateUrl: 'views/assets.html',
+      controller: 'AssetsController'
+    })
+    .state('newReservation', {
+      url: '/newReservation',
+      templateUrl: 'views/newReservation.html',
+      controller: 'NewReservationController'
+    })
+    .state('calendar', {
+      url: '/calendar',
+      templateUrl: 'views/calendar.html',
+      controller: 'calendarController'
+    });
   $locationProvider.html5Mode(true);
 }]);
 
+app.controller('LoginController', ['$scope', function($scope){
+
+}]);
+
 app.controller('HomeController', ['$scope', function($scope){
+
+}]);
+
+app.controller('AssetsController', ['$scope', function($scope){
+
+}]);
+
+app.controller('NewReservationController', ['$scope', function($scope){
+
+}]);
+
+app.controller('calendarController', ['$scope', function($scope){
 
 }]);
